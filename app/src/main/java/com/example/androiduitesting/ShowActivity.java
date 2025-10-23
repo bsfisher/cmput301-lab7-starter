0b445cb0cde46bf2a30db507f465ef949ed98a30
bsfisher <bsfisher@ualberta.ca>
2025-10-23 00:06:22 -0600

    initial solution

---
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -0,0 +1,43 @@
+package com.example.androiduitesting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_show);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView nameField = findViewById(R.id.cityName);
+
+        // Get the Intent that started this Activity
+        Intent intent = getIntent();
+
+        // Retrieve the String argument
+        String cityName = intent.getStringExtra("name");
+        nameField.setText(cityName);
+
+        Button backButton = findViewById(R.id.backShowButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
